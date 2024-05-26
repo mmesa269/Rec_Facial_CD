1fa9c4d250d8f01ca92b7ad031ff2cbdd74a0516
mmesa269 <mmesa269@unab.edu.co>
2024-05-26 15:27:09 -0500

    Ahora si hay imagenes

--- rec_fac.py
+++ rec_fac.py
@@ -1,6 +1,9 @@
 import cv2
 import streamlit as st
+import PIL.Image
+import PIL
 from PIL import Image
+import numpy as np
 
 # Configuración de la página
 st.set_page_config(
@@ -16,41 +19,38 @@ def cargar_modelo():
     return face_recognizer
 
 # Tomar foto
-cap = st.camera_input("Capture una foto para identificar a las personas en ella")   
-faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml') 
+st.subheader("Cargar Foto desde el Computador")
+    
+uploaded_file = st.file_uploader("Elige una imagen...", type=["jpg", "jpeg", "png"])
+imagen_pil = PIL.Image.open(uploaded_file)
+cap = np.array(imagen_pil)
+faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
 if cap is None:
     st.text("Por favor tome una foto")
 else:
-    image = Image.open(cap)
-    st.image(image, use_column_width=True)
-
     # Realizar la predicción
     faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(cap, cv2.COLOR_BGR2GRAY)
     faces = faceClassif.detectMultiScale(gray, 1.3, 5)
+
     for (x, y, w, h) in faces:
         rostro = gray[y:y + h, x:x + w]
         rostro = cv2.resize(rostro, (150, 150), interpolation=cv2.INTER_CUBIC)
+        face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+        face_recognizer.read('modeloLBPHFace2.xml')
         result = face_recognizer.predict(rostro)
 
-        cv2.putText(image, '{}'.format(result), (x, y - 5), 1, 1.3, (255, 255, 0), 1, cv2.LINE_AA)
+        cv2.putText(cap, '{}'.format(result), (x, y - 5), 1, 1.3, (255, 255, 0), 1, cv2.LINE_AA)
         if result[1] < 70:
-            cv2.putText(image, '{}'.format(result[0]), (x, y - 25), 2, 1.1, (0, 255, 0), 1, cv2.LINE_AA)
-            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.putText(cap, '{}'.format(result[0]), (x, y - 25), 2, 1.1, (0, 255, 0), 1, cv2.LINE_AA)
+            cv2.rectangle(cap, (x, y), (x + w, y + h), (0, 255, 0), 2)
         else:
-            cv2.putText(image, 'Desconocido', (x, y - 20), 2, 0.8, (0, 0, 255), 1, cv2.LINE_AA)
-            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            cv2.putText(cap, 'Desconocido', (x, y - 20), 2, 0.8, (0, 0, 255), 1, cv2.LINE_AA)
+            cv2.rectangle(cap, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
     # Convertir imagen de formato OpenCV a formato de imagen PIL
-    img_pil = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    img_pil2 = Image.fromarray((cap))
 
     # Mostrar la imagen capturada
-    st.image(img_pil, caption='Imagen capturada', use_column_width=True)
-
-# Función principal
-def main():
-    # Cargar el modelo de reconocimiento facial
-    face_recognizer = cargar_modelo()
-
-if __name__ == '__main__':
-    main()
+    st.image(img_pil2, caption='Imagen capturada', use_column_width=True)
